d1c1445b2fdd8cb22aff086cc07ffd3cba77c9ff
Silve27 <k.prp0127@gmail.com>
2025-02-08 19:28:47 +0700

    edit hide post button

--- src/app/ui/components/PostWidget.tsx
+++ src/app/ui/components/PostWidget.tsx
@@ -5,7 +5,8 @@ import { FaHeart, FaRegHeart, FaRegCommentDots } from "react-icons/fa";
 import Image from "next/image";
 import EditPostForm from "../post/edit-form";
 import { PostData } from "@/common/interface";
-import { EyeOffIcon } from "lucide-react";
+import { EyeOffIcon, EyeIcon } from "lucide-react";
+import clsx from "clsx";
 
 interface PostProps {
 	user: {
@@ -34,12 +35,12 @@ export default function Post({
 		tags: tags,
 		price: 0,
 		samples: [
-		  {
-			file: undefined,
-			preview: image
-		  }
+			{
+				file: undefined,
+				preview: image
+			}
 		],
-	  })
+	})
 
 	const toggleLike = () => {
 		setLiked(!liked);
@@ -52,86 +53,98 @@ export default function Post({
 
 	return (
 		<>
-			{!hidden && (
-				<div className="card w-full max-w-lg border-2 border-primary bg-white p-4 shadow-xl">
-					{/* Post Header */}
-					<div className="flex items-center justify-between">
-						<div className="flex items-center space-x-3">
-							<Image
-								src={user.avatar}
-								alt="User Avatar"
-								width={40}
-								height={40}
-								className="rounded-md"
-							/>
-							<div>
-								<p className="font-semibold">{user.name}</p>
-								<p className="text-xs text-gray-500">
-									{timestamp}
-								</p>
-							</div>
-						</div>
-						{/* Edit Post Button */}
-						{/* <button className="btn btn-ghost btn-sm">⋮</button> */}
-						<div className="flex gap-6">
-							<EyeOffIcon
-								className="my-3 cursor-pointer hover:text-red-600"
-								onClick={() => setHidden(true)}
-							/>
-							<EditPostForm post={data} setPost={setData} />
+			<div className="card w-full max-w-lg border-2 border-primary bg-white p-4 shadow-xl">
+				{/* Post Header */}
+				<div className="flex items-center justify-between">
+					<div className="flex items-center space-x-3">
+						<Image
+							src={user.avatar}
+							alt="User Avatar"
+							width={40}
+							height={40}
+							className="rounded-md"
+						/>
+						<div>
+							<p className="font-semibold">{user.name}</p>
+							<p className="text-xs text-gray-500">
+								{timestamp}
+							</p>
 						</div>
 					</div>
-
-					{/* Tags */}
-					<div className="mt-3 flex flex-wrap gap-1">
-						{data.tags.map((tag) => (
-							<div
-								key={tag}
-								className="mb-1 flex items-center rounded-full bg-neutral px-2 py-1 text-white"
-							>
-								{tag}
-							</div>
-						))}
-					</div>
-
-					{/* Post Content */}
-					<p className="mt-2 text-gray-800">{data.description}</p>
-					{/* Display multiple images */}
-					{data.samples.length > 0 && (
-						<div className={`mt-3 grid gap-3 ${data.samples.length === 1 ? "grid-cols-1" : "grid-cols-2"}`}>							{data.samples.map((sample, index) => (
-							<div key={index} className="overflow-hidden rounded-lg">
-								<Image
-								src={sample?.preview || '/path/to/default/image.jpg'}
-								alt={`Post Image ${index + 1}`}
-								width={500}
-								height={300}
-								className="rounded-lg"
+					<div className="flex gap-6">
+						{/* Eyes off */}
+						{ 	hidden &&
+							<div className="flex gap-2 cursor-pointer hover:text-red-600" onClick={() => setHidden(!hidden)}> 
+								<EyeOffIcon
+									className="my-3"
 								/>
+								<p className="font-semibold py-3">hidden</p>
 							</div>
-							))}
-						</div>
-					)}
+						}
 
-					{/* Post Actions */}
-					<div className="mt-3 flex items-center space-x-4">
-						<button
-							className="btn btn-ghost btn-sm flex items-center space-x-2"
-							onClick={toggleLike}
-						>
-							{liked ? (
-								<FaHeart className="h-5 w-5 text-red-500" />
-							) : (
-								<FaRegHeart className="h-5 w-5 text-gray-600" />
-							)}
-							<span>{likes}</span>
-						</button>
-						<button className="btn btn-ghost btn-sm flex items-center space-x-2">
-							<FaRegCommentDots className="h-5 w-5 text-gray-600" />
-							<span>Comment</span>
-						</button>
+						{/* Eyes on */}
+						{ 	!hidden &&
+							<div className="flex gap-2 cursor-pointer hover:text-red-600" onClick={() => setHidden(!hidden)}> 
+								<EyeIcon
+									className="my-3"
+								/>
+								<p className="font-semibold py-3">public</p>
+						</div>
+						}
+						{/* Edit Post Button */}
+						<EditPostForm post={data} setPost={setData} />
 					</div>
 				</div>
-			)}
+
+				{/* Tags */}
+				<div className="mt-3 flex flex-wrap gap-1">
+					{data.tags.map((tag) => (
+						<div
+							key={tag}
+							className="mb-1 flex items-center rounded-full bg-neutral px-2 py-1 text-white"
+						>
+							{tag}
+						</div>
+					))}
+				</div>
+
+				{/* Post Content */}
+				<p className="mt-2 text-gray-800">{data.description}</p>
+				{/* Display multiple images */}
+				{data.samples.length > 0 && (
+					<div className={`mt-3 grid gap-3 ${data.samples.length === 1 ? "grid-cols-1" : "grid-cols-2"}`}>							{data.samples.map((sample, index) => (
+						<div key={index} className="overflow-hidden rounded-lg">
+							<Image
+							src={sample?.preview || '/path/to/default/image.jpg'}
+							alt={`Post Image ${index + 1}`}
+							width={500}
+							height={300}
+							className="rounded-lg"
+							/>
+						</div>
+						))}
+					</div>
+				)}
+
+				{/* Post Actions */}
+				<div className="mt-3 flex items-center space-x-4">
+					<button
+						className="btn btn-ghost btn-sm flex items-center space-x-2"
+						onClick={toggleLike}
+					>
+						{liked ? (
+							<FaHeart className="h-5 w-5 text-red-500" />
+						) : (
+							<FaRegHeart className="h-5 w-5 text-gray-600" />
+						)}
+						<span>{likes}</span>
+					</button>
+					<button className="btn btn-ghost btn-sm flex items-center space-x-2">
+						<FaRegCommentDots className="h-5 w-5 text-gray-600" />
+						<span>Comment</span>
+					</button>
+				</div>
+			</div>
 		</>
 	);
 }
